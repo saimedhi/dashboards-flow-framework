1bffbc831ac5cf7174d7af50a4ab37b5b7d12b9b
saimedhi <saimedhi@amazon.com>
2024-09-04 12:11:24 -0700

    tests
    Signed-off-by: saimedhi <saimedhi@amazon.com>

--- public/pages/workflow_detail/workflow_detail.test.tsx
+++ public/pages/workflow_detail/workflow_detail.test.tsx
@@ -9,6 +9,7 @@ import {
 } from 'react-router-dom';
 import { WorkflowDetail } from './workflow_detail';
 import { WorkflowDetailRouterProps } from '../../pages';
+import '@testing-library/jest-dom';
 
 jest.mock('../../services', () => {
   const { mockCoreServices } = require('../../../test');
@@ -83,7 +84,6 @@ const mockStore = {
             },
           },
           lastUpdated: 1725413687437,
-          state: 'Not started',
           resourcesCreated: [],
         },
       },
@@ -114,9 +114,28 @@ const renderWithRouter = (initialEntries: string[]) =>
 describe('WorkflowDetail', () => {
   test('renders the page with workflowId parameter', () => {
     const workflowId = '12345';
-    const { getAllByText } = renderWithRouter([`/workflow/${workflowId}`]);
-    console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$');
+    const { getAllByText, getByText, getByRole } = renderWithRouter([
+      `/workflow/${workflowId}`,
+    ]);
 
-    expect(getAllByText('test_workflow abcd').length).toBeGreaterThan(0);
+    expect(getAllByText('test_workflow').length).toBeGreaterThan(0);
+    expect(getAllByText('Create an ingest pipeline').length).toBeGreaterThan(0);
+    expect(getAllByText('Skip ingestion pipeline').length).toBeGreaterThan(0);
+    expect(getAllByText('Define ingest pipeline').length).toBeGreaterThan(0);
+    expect(getAllByText('Tools').length).toBeGreaterThan(0);
+    expect(getAllByText('Preview').length).toBeGreaterThan(0);
+    expect(getAllByText('Not started').length).toBeGreaterThan(0);
+    expect(
+      getAllByText('Last updated: 09/03/24 06:34 PM').length
+    ).toBeGreaterThan(0);
+    expect(getAllByText('Search pipeline').length).toBeGreaterThan(0);
+    expect(getByText('Close')).toBeInTheDocument();
+    expect(getByText('Export')).toBeInTheDocument();
+    expect(getByText('Visual')).toBeInTheDocument();
+    expect(getByText('JSON')).toBeInTheDocument();
+    expect(getByRole('tab', { name: 'Run ingestion' })).toBeInTheDocument();
+    expect(getByRole('tab', { name: 'Run queries' })).toBeInTheDocument();
+    expect(getByRole('tab', { name: 'Errors' })).toBeInTheDocument();
+    expect(getByRole('tab', { name: 'Resources' })).toBeInTheDocument();
   });
 });
